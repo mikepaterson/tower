34e506722c5677688f1ca38088a54f0348304a4f
Mike Paterson <mike@firestitch.com>
2023-03-30 17:46:56 -0400

    coins, sounds, cleanup

--- farm.js
+++ farm.js
@@ -10,11 +10,16 @@ class Farm {
     this.payoutCooldown = farmData.payoutCooldown;
     this.cost = farmData.cost;
     this.lastPayoutTime = 0;
+  }
 
-
+  spawn(currentTime) {
   }
 
   update(currentTime) {
+    if(this.lastPayoutTime===0) {
+      this.lastPayoutTime = currentTime;
+    }
+
     if (currentTime > this.lastPayoutTime  + (this.payoutCooldown * 1000)) {
       //payout
       this.game.spawnCoins(this.payoutCoins, {...this.gridPosition});
